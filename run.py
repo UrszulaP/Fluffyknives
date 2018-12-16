c43e1076a5589c0180dcf51fe769b24d1274e2aa
UrszulaP <urszulapoliszuk@gmail.com>
2018-12-16 13:31:26 +0100

    31 - debug mode False

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app_files import app
 
 if __name__ == '__main__':
-	app.run(debug=True, use_debugger=False)
+	app.run(debug=False, use_debugger=False)
 
 # Przy publikowaniu aplikacji należy wyłączyć debug mode
 # Względy bezpieczeństwa, przy powstaniu błędu użytkownicy nie będą mieli
